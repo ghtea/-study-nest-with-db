f7770e34862a68cabf2a59aeb62e3066b0688e83
ghtea <eiirwp@gmail.com>
2021-03-18 14:48:47 +0900

    failing to connect with postgres db in aws rds

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,48 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
+//import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(typeOrmConfig),
-    TasksModule
+    TasksModule,
+
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: async () => ({
+        type: 'postgres' as 'postgres',
+        host: process.env.RDS_HOSTNAME,
+        port: parseInt(process.env.RDS_PORT),
+        username: process.env.RDS_USERNAME,
+        password: process.env.RDS_PASSWORD,
+        database: process.env.RDS_DB_NAME,
+        autoLoadEntities: true,
+        synchronize: process.env.TYPEORM_SYNC === 'true'
+      }),
+    }),
+
   ],
 })
 export class AppModule {}
+
+// https://jaketrent.com/post/configure-typeorm-inject-nestjs-config
+
+
+// TypeOrmModule.forRoot(typeOrmConfig),
+
+/*
+TypeOrmModule.forRootAsync({
+  imports: [],
+  inject: [],
+  useFactory: async () => ({
+    type: 'postgres' as 'postgres',
+    host: process.env.RDS_HOSTNAME,
+    port: parseInt(process.env.RDS_PORT),
+    username: process.env.RDS_USERNAME,
+    password: process.env.RDS_PASSWORD,
+    database: process.env.RDS_DB_NAME,
+  }),
+}),
+*/
+
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import {Logger} from '@nestjs/common';
 import { AppModule } from './app.module';
-import * as config from 'config';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
-  const serverConfig = config.get('server');
-
   const app = await NestFactory.create(AppModule);
-  
-  const port = process.env.PORT || serverConfig.port;
+
+  if (process.env.NODE_ENV === 'development'){
+    app.enableCors();
+  }
+  else {
+    app.enableCors({origin: process.env.origin });
+    logger.log(`Acceping requests from "${process.env.origin}"`)
+  }
+
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`)
+  logger.log(`Using db: ${process.env.RDS_HOSTNAME}`)
 }
 bootstrap();
